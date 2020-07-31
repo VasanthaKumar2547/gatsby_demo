b5b695590fcad7b764dd43915ac4195f0f44b62f
Vasanth RS <vasanthakumar3826@mail.com>
2020-07-31 18:15:42 +0530

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { HeaderNav, Logo, Menu } from "../styles/headerStyles"
+import { Container, Flex } from "../styles/globalStyles"
+import { Link } from "gatsby"
+
+const Header = () => {
+  return (
+    <HeaderNav>
+      <Container>
+        <Flex spaceBetween noHeight>
+          <Logo>
+            <Link to="/">FURR</Link>
+            <span></span>
+            <Link to="/">W</Link>
+          </Logo>
+          <Menu>
+            <button>
+              <span></span>
+              <span></span>
+            </button>
+          </Menu>
+        </Flex>
+      </Container>
+    </HeaderNav>
+  )
+}
+
+export default Header
